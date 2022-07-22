cccfb867f8e2149904ac05a8872b2f3e5e746186
NicMortelliti <npmortelliti@gmail.com>
2022-07-22 10:35:44 -0700

    Assignment complete

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,8 +28,18 @@ def long_planeteer_calls(words)
   result
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  first_cheese = nil
+
+  cheese_types.each do |cheese|
+    if arr.include?(cheese)
+      first_cheese = cheese
+    end
+  end
+
+  first_cheese
 end
 
