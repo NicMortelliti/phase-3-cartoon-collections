791b535e97f9bae7a7c089caef2993540404441e
NicMortelliti <npmortelliti@gmail.com>
2022-07-22 10:31:04 -0700

    Solved long_planeteer_calls

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(dwarves)
   dwarves.each.with_index { |val, i| puts "#{i + 1}. #{val}\n"}
 end
@@ -9,11 +11,25 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+
+  # Initialze 'result' to false
+  result = false
+
+  words.each do |word|
+    
+    # If a word > 4 is found, set result to true
+    if word.length > 4
+      result = true
+    end
+  end
+
+  # Return result
+  result
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
